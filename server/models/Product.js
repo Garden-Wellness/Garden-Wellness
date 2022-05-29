59df6c6247ea8f25454c8aaed5b9b94e1dcc1cec
Maxiel Mrvaljevic <maxielmrvaljevic@Maxiels-Air.lan>
2022-05-29 19:33:21 -0400

    creates product model

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,5 +1,25 @@
 const Sequelize = require('sequelize');
-const db = require('../db');
+const db = require('../db/db');
 
-
-
+module.exports = db.define('product', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    }, 
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: true
+    },
+    image_url: {
+        type: Sequelize.STRING,
+        defaultValue: '../../public/images/coming_soon.jpg',
+    },
+    inventory_quantity: Sequelize.INTEGER
+});
