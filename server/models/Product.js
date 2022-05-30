051f41c3db07b04a2dd718188943aa4ccf1ea711
Maxiel Mrvaljevic <maxielmrvaljevic@Maxiels-Air.lan>
2022-05-29 20:18:29 -0400

    configures spacingin db and models

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,25 +1,25 @@
-const Sequelize = require('sequelize');
-const db = require('../db/db');
+const Sequelize = require("sequelize");
+const db = require("../db/db");
 
-module.exports = db.define('product', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
-    }, 
-    price: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
+module.exports = db.define("product", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     },
-    description: {
-        type: Sequelize.TEXT,
-        allowNull: true
-    },
-    image_url: {
-        type: Sequelize.STRING,
-        defaultValue: '../../public/images/coming_soon.jpg',
-    },
-    inventory_quantity: Sequelize.INTEGER
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: true,
+  },
+  image_url: {
+    type: Sequelize.STRING,
+    defaultValue: "../../public/images/coming_soon.jpg",
+  },
+  inventory_quantity: Sequelize.INTEGER,
 });
